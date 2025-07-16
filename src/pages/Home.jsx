65cf5e73b8d55f3c05fc7b02847fa90cd5496b1a
Machfudin <machfudin37@gmail.com>
2025-07-16 14:27:21 +0700

    feat: add Navbar component in Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,12 @@
+import Navbar from '@/components/Navbar';
 export default function Home() {
   return (
-    <div className='border-white shadow-inner mt-8 mx-10 rounded-2xl p-4'>
-      <h1 className='text-2xl font-bold'>PokéAPI</h1>
-      <p>Selamat datang! Pilih menu di atas untuk mulai menjelajah data Pokémon.</p>
-    </div>
+    <>
+      <Navbar />
+      <div className='bg-gray-100 shadow-inner mt-8 mx-10 rounded-2xl p-4'>
+        <h1 className='text-2xl font-bold'>PokéAPI</h1>
+        <p>Selamat datang! Pilih menu di atas untuk mulai menjelajah data Pokémon.</p>
+      </div>
+    </>
   );
 }
