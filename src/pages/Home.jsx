cccbd1ad3912a002065a3f017c040d1d3f161950
Machfudin <machfudin37@gmail.com>
2025-07-16 13:00:45 +0700

    feat: styling home content

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div>
-      <h1 className='text-2xl font-bold'>PokéAPI Explorer</h1>
+    <div className='border-white shadow-inner mt-8 mx-10 rounded-2xl p-4'>
+      <h1 className='text-2xl font-bold'>PokéAPI</h1>
       <p>Selamat datang! Pilih menu di atas untuk mulai menjelajah data Pokémon.</p>
     </div>
   );
